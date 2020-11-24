4de2a4e26be9a913ce4dc6a95ac3f4fd2e9f531a
wst24365888 <hansen0715.2@gmail.com>
2020-11-24 11:30:34 +0800

    update views

--- C8763/getImage.py
+++ C8763/getImage.py
@@ -5,10 +5,12 @@ from linebot import LineBotApi
 from django.conf import settings
 
 def getImage(messageID):
-    r = requests.get(
-        'https://api.line.me/v2/bot/message/{messageID}/content', 
-        headers = "Authorization: Bearer {}".format(LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN))
-    )
+    print(LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN))
+    # 自訂表頭
+    my_headers = {'Authorization': "Bearer {}".format(LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN))}
+
+    # 將自訂表頭加入 GET 請求中
+    r = requests.get('https://api.line.me/v2/bot/message/{messageID}/content', headers = my_headers)
 
     print(r)
     print("aaa")
