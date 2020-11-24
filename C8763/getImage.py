7fa0e31e92e465309294a181cc81ad6f4ff1b7a9
wst24365888 <hansen0715.2@gmail.com>
2020-11-24 11:40:35 +0800

    update views

--- C8763/getImage.py
+++ C8763/getImage.py
@@ -4,11 +4,12 @@ from NCUAI_LineBot import settings
 
 def getImage(messageID):
     print(settings.LINE_CHANNEL_ACCESS_TOKEN)
+    print(messageID)
     # 自訂表頭
     my_headers = {'Authorization': "Bearer {}".format(settings.LINE_CHANNEL_ACCESS_TOKEN)}
 
     # 將自訂表頭加入 GET 請求中
-    r = requests.get('https://api.line.me/v2/bot/message/{messageID}/content', headers = my_headers)
+    r = requests.get("https://api.line.me/v2/bot/message/{}/content".format(messageID), headers = my_headers)
 
     print(r)
     print("aaa")
